042f80a77bc1b97e1d5380402abe71d7401aa1be
marsha17 <marsha17@seattleu.edu>
2016-06-27 20:32:40 -0400

    Revised 2show comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,16 @@
 function FizzBuzz(){
+  // For each number less than 20 //
     for(var i=1; i<=20; i++){
+      //If number is divisible by 3 AND 5, print FizzBuzz //
         if(i % 3 === 0 && i % 5 === 0){
             console.log(i+'FizzBuzz');
+          //ELSE, IF number is divisible by 3, print Fizz //
         } else if(i % 3 === 0){
             console.log(i+' Fizz');
+          //ELSE, IF number is divisible by 5, print Buzz //
         } else if(i % 5 === 0){
             console.log(i+' Buzz');
+          //ELSE, print number
         } else {
             console.log(i);    
         }
